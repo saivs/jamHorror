891899e4c0c257d7c5b8a53e5a1a227ad55e9ac3
Katunator <hinckel.katia@gmail.com>
2022-11-26 22:32:43 +0300

    base logic for level (items, player, level controller)

---
+++ Assets/Scripts/Configs/CanConfig.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/" + nameof(CanConfig))]
+public class CanConfig : ScriptableObject
+{
+    [SerializeField] private Color _color;
+
+    public Color Color => _color;
+}
