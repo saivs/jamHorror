dadd1b9b02703085c0bd78dfb0cedb93701d8c2b
DESKTOP-71DSOQ8\Ноутбук <l124rd@mail.ru>
2022-11-27 21:23:58 +0300

    Menu

---
+++ Assets/Scenes/GoToGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeScene(){
+        SceneManager.LoadScene("Level");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
