0cb4038b35356d157ed8235cb6c9a46126d25028
Katunator <hinckel.katia@gmail.com>
2022-11-27 23:29:06 +0300

    electric panel and basement

---
+++ Assets/Scripts/Services/Level/Environment/Objects/ElectricPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricPanel: MonoBehaviour
+{
+    [SerializeField] private Basement _basement;
+    [SerializeField] private ElectricPanelConfig _config;
+    [SerializeField] private List<ElectricPanelSwitcher> _switchers;
+
+    private int _pressedSwitchersCount;
+
+    private void Awake()
+    {
+        _pressedSwitchersCount = 0;
+
+        for (int i = 0; i < _switchers.Count; i++)
+        {
+            int index = i;
+            _switchers[i].OnPressed += () => OnSwitcherPressed(index);
+        }
+    }
+
+    private void OnSwitcherPressed(int index)
+    {
+        if (_config.SwitcherIndexesToPress[_pressedSwitchersCount] == index)
+        {
+            _pressedSwitchersCount++;
+
+            if (_pressedSwitchersCount == _switchers.Count)
+            {
+                TurnOnLight();
+            }
+
+            return;
+        }
+
+        KillPlayer();
+    }
+
+    private void TurnOnLight()
+    {
+        _basement.TurnOnLight();
+    }
+
+    private void KillPlayer()
+    {
+        Player.Instance.Kill();
+    }
+}
---
+++ Assets/Scripts/Services/Level/Environment/Objects/ElectricPanelSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class ElectricPanelSwitcher : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Transform _buttonRotatePivot;
+    [SerializeField] private Transform _button;
+
+    private bool _pressed;
+
+    public event Action OnPressed; 
+
+    public void Interact()
+    {
+        if (_pressed)
+        {
+            return;
+        }
+
+        _pressed = true;
+        SwitchDown();
+        OnPressed?.Invoke();
+    }
+
+    private void SwitchDown()
+    {
+        float angle = -_button.transform.localRotation.eulerAngles.z * 2;
+        _button.RotateAround(_buttonRotatePivot.position, Vector3.forward, angle);
+    }
+}
