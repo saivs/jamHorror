10d983a622f931187f7d4b569d203e500b9c9005
Katunator <hinckel.katia@gmail.com>
2022-11-30 03:30:34 +0300

    sounds, brush clear and color

--- Assets/Scripts/Services/Level/Environment/Objects/Can.cs
+++ Assets/Scripts/Services/Level/Environment/Objects/Can.cs
@@ -2,19 +2,11 @@
 
 public class Can : MonoBehaviour, IInteractable
 {
-    [SerializeField] private CanConfig _config;
-    [SerializeField] private MeshRenderer _meshRenderer;
-
-    private void Start()
-    {
-        _meshRenderer.material.color = _config.Color;
-    }
-
     public void Interact()
     {
         if (Player.Instance.Item is Brush brush)
         {
-            brush.Paint(_config.Color);
+            brush.Paint();
             return;
         }
     }
