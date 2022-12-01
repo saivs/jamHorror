38f7aded46f1c8b1fe9cc9dd0a8c2b8f47fb9bd4
Katunator <hinckel.katia@gmail.com>
2022-12-01 23:21:57 +0300

    ui font sprites

--- Assets/Scripts/GoToGame.cs
+++ Assets/Scripts/GoToGame.cs
@@ -5,19 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GoToGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void ChangeScene(){
         SceneManager.LoadScene("Game");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void Exit()
     {
-        
+        Application.Quit();
     }
 }
